26f07fb29755b60497975fb1be8fb511885780fd
Ambidre <mstitelnica1999@gmail.com>
2022-07-10 14:39:17 +0700

    fixing tests in WithProblemsTests

--- src/test/java/WithProblemsTests.java
+++ src/test/java/WithProblemsTests.java
@@ -1,54 +1,52 @@
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class WithProblemsTests {
 
-    private final String CONST = "const";
+    private String CONST = "const";
     private final static String bd = "pui";
 
+    //Переименование первого и третьего кейса
     @Test
-    public void equalsOneToOne(){
+    public void equalsOneToOneString(){
         assertEquals("1", "1");
     }
 
-
+    //Убран final, т.к. в этом кейсе значение переменной меняется
     @Test
     public void assignValueToConstVar(){
         CONST = "newValue";
         assertEquals("newValue", CONST);
     }
 
+    //Переименование первого и третьего кейса
     @Test
-    public static void equalsOneToOne(){
+    public void equalsOneToOne(){
         assertEquals(1, 1);
     }
-
-
 
     @Test
     public void stringsMustBeEquals(){
         String res = "a";
 
-        if (bd == new String("pui")) {
+        if (bd == "pui") {
             res = "asd";
         }
 
         assertEquals("asd", res);
     }
 
+    //Тест реализован согласно его названию, удаление первого элемента из списка
     @Test
     public void successfullyRemovingFirstElementFromList(){
-        List<String> sourceData = List.of("1", "viskas", "chupocabra");
-        for (String element: sourceData){
-            sourceData.remove(element);
-        }
+        List<String> sourceData = new LinkedList<>(Arrays.asList("1", "viskas", "chupocabra"));
+        sourceData.remove(0);
         assertFalse(sourceData.contains("1"));
     }
-
-
 }
